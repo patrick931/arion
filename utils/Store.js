2f94e95d2cea92ae52d5c788e91855e84e329a36
patrickkamau@mac.com <patrickkamaumac.com@KamauJOMs-MacBook-Pro.local>
2022-05-18 14:26:17 +0300

    clear shipping address on logut

--- utils/Store.js
+++ utils/Store.js
@@ -54,7 +54,11 @@ function reducer(state, action) {
     case 'USER_LOGIN':
       return { ...state, userInfo: action.payload };
     case 'USER_LOGOUT':
-      return { ...state, userInfo: null, cart: { cartItems: [] } };
+      return {
+        ...state,
+        userInfo: null,
+        cart: { cartItems: [], shippingAddress: [] },
+      };
 
     default:
       return state;
